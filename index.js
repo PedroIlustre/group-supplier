e695065027c22791602b7d382f14847f9872d491
Pedro Ilustre <pedro.ilustre.web@gmail.com>
2024-02-02 23:45:00 -0300

    feat: add cors

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const express = require('express');
 const fs = require('fs');
 const csv = require('csv-parser');
 const _ = require('lodash');
+const cors = require('cors');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(cors());
 app.use(express.json());
 
 app.post('/process', async (req, res) => {
